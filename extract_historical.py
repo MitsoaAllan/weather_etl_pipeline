4d3f01b46a154574391d9ef390f7c4bf8cf1fe9f
MitsoaAllan <hei.mitsoa@gmail.com>
2025-07-20 18:46:53 +0300

    fix : Add condition

--- extract_historical.py
+++ extract_historical.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import os
 
 def extract_historical():
     """
     Charge les données historiques météo depuis un fichier CSV.
     """
-    df = pd.read_csv("/home/mitsoa/airflow/weather_pipeline/dags/data/cleaned_historical_data/historical_weather.csv", parse_dates=["date"])
-    df.to_csv("/home/mitsoa/airflow/weather_pipeline/dags/data/extracted_historical.csv", index=False)
+    output_path = "/home/mitsoa/airflow/weather_pipeline/dags/data/extracted_historical.csv"
+
+    if os.path.exists(output_path):
+        print("Fichier déjà extrait. Lecture sans réécriture.")
+        df = pd.read_csv(output_path, parse_dates=["date"])
+    else:
+        df = pd.read_csv("/home/mitsoa/airflow/weather_pipeline/dags/data/cleaned_historical_data/historical_weather.csv", parse_dates=["date"])
+        df.to_csv(output_path, index=False)
+        print("✅ Données extraites et enregistrées.")
