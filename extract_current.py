0ba4d4e08abbf2e6f1cb7b09229acf9937e7bfd0
MitsoaAllan <hei.mitsoa@gmail.com>
2025-07-20 18:55:50 +0300

    fix: Change open_weather_config.json path, round temperature and load the current weather into 'weather-{today}.csv file

--- extract_current.py
+++ extract_current.py
@@ -8,12 +8,13 @@ def extract_current():
     Récupère les données météo actuelles depuis l'API OpenWeather pour 5 villes.
     """
 
-    with open("openweather_config.json", "r") as f:
+    with open("/home/mitsoa/airflow/weather_pipeline/dags/scripts/openweather_config.json", "r") as f:
         config = json.load(f)
 
     api_key = config["api_key"]
     cities = config["cities"]
     
+    today = datetime.utcnow().date().isoformat()
 
     data = []
 
@@ -24,16 +25,18 @@ def extract_current():
         response = requests.get(url)
         if response.status_code == 200:
             weather = response.json()
+            temp=weather["main"]["temp"] - 273.15;
             data.append({
                 "date": datetime.utcnow().date(),
-                "city": name,
-                "temp": weather["main"]["temp"] - 273.15,
+                "temp":round(temp,2) ,
                 "humidity": weather["main"]["humidity"],
                 "pressure": weather["main"]["pressure"],
-                "wind": weather["wind"]["speed"]
+                "wind": weather["wind"]["speed"],
+                "city": name
             })
         else:
             print(f"Échec pour {name} : {response.text}")
 
     df = pd.DataFrame(data)
     df.to_csv("/home/mitsoa/airflow/weather_pipeline/dags/data/current_data.csv", index=False)
+    df.to_csv(f"/home/mitsoa/airflow/weather_pipeline/dags/data/processed/weather_{today}.csv", index=False)
